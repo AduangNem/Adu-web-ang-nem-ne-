a1fbda93a8fcf1835e59b3f19f3edde6e7766647
Adu ang Nem <vuonglinh280406.qv@gmail.com>
2025-03-20 14:48:59 +0700

    Add files via upload

---
+++ layouts/master/footer.php
@@ -0,0 +1,51 @@
+<footer class="text-center text-lg-start bg-light text-muted footer">
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"></section>
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <div class="row mt-3">
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Selling Zuki glasses
+                        </h6>
+                        <p>
+                            Specializing in selling glasses frames and lenses
+                        </p>
+                    </div>
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Product
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Glasses frames</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Lenses</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Sunglasses</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">
+
+                            Useful links
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">FAQs</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">About us</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Contact
+                        </h6>
+                        <p><i class="fas fa-home me-3"> Da Ton, Gia Lam, Ha Noi, Viet Nam</i></p>
+                        <p><i class="fas fa-envelope me-3"> binh63261@gmail.com</i></p>
+                        <p><i class="fas fa-phone me-3">  +84 354497902</i></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
---
+++ layouts/master/header.php
@@ -0,0 +1,63 @@
+<header>
+        <!-- navbar -->
+        <div class="menu">
+            <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+                <div class="container">
+                    <div class="row">
+                        <div style="margin-top: -20px; margin-left: 40px;">
+                            <nav class="navbar">
+                                <!-- Logo -->
+                                <a class="navbar-brand" href="/admin/main.html">
+                                    <img src="images/logo.png" alt="Kính mắt Zuki" width="100" height="auto">
+                                </a>
+                                <div style="margin-left: 160px;">
+                                    <a id="item" href="/admin/main.html">Home</a>
+                                </div>
+                                <div style="margin-left: 60px;">
+                                    <a id="item" href="/layouts/products/glasses_frames.html">Glasses frames</a>
+                                </div>
+                                <div style="margin-left: 60px;">
+                                    <a id="item" href="/layouts/products/Lenses.html">Lenses</a>
+                                </div>
+                                <div style="margin-left: 60px;">
+                                    <a id="item" href="/layouts/products/Sunglasses.html">Sunglasses</a>
+                                </div>
+
+                                <!-- Search bar -->
+                                <div style="margin-left: 180px;">
+                                    <nav class="navbar">
+                                        <form method="post" class="d-flex" role="search">
+                                            <button id="search" class="btn btn-outline-success" type="submit"><img
+                                                    src="images/UI_Icon_Search.png" alt=""></button>
+                                        </form>
+                                    </nav>
+                                </div>
+
+                                <!-- Login/Register -->
+                                <div style="margin-left: 40px;">
+                                    <a id="login" href="#">Login/Register</a>
+                                </div>
+                                <input type="text" id="search_input" placeholder="Search..." style="display: none;">
+                            </nav>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- js for search tab -->
+                <script>
+                    const searchBtn = document.getElementById('search');
+                    const searchInput = document.getElementById('search_input');
+
+                    searchBtn.addEventListener('mouseover',
+                        () => {
+                            searchInput.style.display = 'inline';
+                            searchInput.focus();
+                        });
+
+                    searchBtn.addEventListener('mouseout', () => {
+                        searchInput.style.display = 'none';
+                    });
+                </script>
+            </nav>
+        </div>
+    </header>
